e282f655cae02d39b26483e41752d38155b7bfe6
Udita <153092248+udita-0707@users.noreply.github.com>
2025-01-07 11:38:33 +0530

    Added Payment and subscription

--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -1,19 +1,28 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { Button } from '@/components/ui/button'
+import { useSubscription } from '@/hooks/use-subscription'
+import { CreditCardIcon, Loader2 } from 'lucide-react'
+import React from 'react'
 
-type Props = {};
+type Props = {}
 
 const PaymentButton = (props: Props) => {
+  const { onSubscribe, isProcessing } = useSubscription()
   return (
     <Button
-      className="bg-gradient-to-br 
-    text-white rounded-full 
-    from-[#9685DB] 
-    to-[#9434E6] font-bold to-[#CC3BD4]"
+      disabled={isProcessing}
+      onClick={onSubscribe}
+      className="bg-gradient-to-br
+     text-white 
+     rounded-full 
+    from-[#6d60a3] 
+    via-[#9434E6] 
+    font-bold 
+    to-[#CC3BD4]"
     >
+      {isProcessing ? <Loader2 className="animate-spin" /> : <CreditCardIcon />}
       Upgrade
     </Button>
-  );
-};
+  )
+}
 
-export default PaymentButton;
+export default PaymentButton
